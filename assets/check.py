494a4fdb743bb4083ae22faed194892edc998a9d
apjansing <apjansing@gmail.com>
2020-03-18 23:15:03 -0400

    added option for passing username and password

--- assets/check.py
+++ assets/check.py
@@ -2,18 +2,24 @@
 import json
 import sys
 import pymongo as pm
-from bson.json_util import dumps as mongo_dumps
 from pymongo.errors import ConnectionFailure
 from urllib.parse import quote_plus
+
+def get_or_default(dictionary, key, default=''):
+  try:
+    return dictionary[key]
+  except:
+    return default
 
 def msg(msg, *args, **kwargs):
     print(msg.format(*args, **kwargs), file=sys.stderr)
 
-def _check(instream, username="admin", 
-                    password="admin"):
+def _check(instream):
     payload = json.load(instream)
     msg('Payload {}', payload)
     source = payload["source"]
+    username=get_or_default(source, "username", "admin")
+    password=get_or_default(source, "password", "admin")
     host=f'{source["url"]}:{source["port"]}'
     uri = "mongodb://%s:%s@%s" % (quote_plus(username), 
                                         quote_plus(password), host)
@@ -26,20 +32,15 @@ def _check(instream, username="admin",
         connection.admin.command('ismaster')
     except ConnectionFailure:
         msg("Server not available")                                        
-    # print(connection.test.trigger.find().next())
     db = connection[source['db']]
 
     collection = db[source['collection']]
     find = source['find']
     cursor = collection.find(dict(find))
     for _ in range(cursor.count()):
-        # print(mongo_dumps(cursor.next()))
         current = cursor.next()
-        return [{"versions" : str(current["_id"])}]
+        return [{"version" : str(current["_id"])}]
     
 
 if __name__ == "__main__":
     print(json.dumps(_check(sys.stdin)))
-    # with open('secret.json', 'rb') as f:
-    #     # print(_check(f))
-    #     print(_check(f))
