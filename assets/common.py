e0760b6ba5372a08d55cf1836862cb54c2a25a59
apjansing <apjansing@gmail.com>
2020-03-30 00:12:50 -0400

    Added get_payload_data to common

--- assets/common.py
+++ assets/common.py
@@ -1,5 +1,6 @@
 import sys
 from pprint import pprint
+from urllib.parse import quote_plus
 
 def get_or_default(dictionary, key, default=''):
   try:
@@ -12,3 +13,11 @@ def msg(msg, *args, **kwargs):
     pprint(msg, stream=sys.stderr)
   else:
     print(msg.format(*args, **kwargs), file=sys.stderr)
+
+def get_payload_data(payload):
+  source = payload["source"]
+  username = get_or_default(source, "username", "admin")
+  password = get_or_default(source, "password", "admin")
+  host = f'{source["url"]}:{source["port"]}'
+  uri = "mongodb://%s:%s@%s" % (quote_plus(username), quote_plus(password), host)
+  return source, uri
