11d814970d81e3a6a9f5388447f320ecd930d2e9
apjansing <apjansing@gmail.com>
2020-03-30 00:13:23 -0400

    using common.py

--- assets/check.py
+++ assets/check.py
@@ -5,20 +5,7 @@ import os
 import pymongo as pm
 from pymongo.errors import ConnectionFailure
 from urllib.parse import quote_plus
-from pprint import pprint
-from bson import ObjectId
-
-def get_or_default(dictionary, key, default=''):
-  try:
-    return dictionary[key]
-  except:
-    return default
-
-def msg(msg, *args, **kwargs):
-  if isinstance(msg, dict):
-    pprint(msg, stream=sys.stderr)
-  else:
-    print(msg.format(*args, **kwargs), file=sys.stderr)
+from common import get_or_default, msg, get_payload_data
 
 def get_versions(version):
   if os.path.exists('versions.json'):
@@ -47,11 +34,8 @@ def _check(instream):
   # if len(versions['versions']) > 2:
   #   raise Exception
 
-  username=get_or_default(source, "username", "admin")
-  password=get_or_default(source, "password", "admin")
-  host=f'{source["url"]}:{source["port"]}'
-  uri = "mongodb://%s:%s@%s" % (quote_plus(username), 
-                                      quote_plus(password), host)
+  source, uri = get_payload_data(payload)
+
   connection = pm.MongoClient(uri)
   msg('Connection {}', connection)
   try:
